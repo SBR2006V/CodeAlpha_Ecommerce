1e5e86e46d657b4ef19c139f44d6f0b5c149fb72
SBR2006V <biswassourasish905athlite@gmail.com>
2026-05-17 20:16:55 +0530

    Fix deployed API routes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://codealpha-ecommerce-rppe.onrender.com",
+  baseURL: "https://codealpha-ecommerce-rppe.onrender.com/api",
 });
 
 export default API;
