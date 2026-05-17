050a3bb54d4e7d46c90e77fcb630cc4c09d1ab49
SBR2006V <biswassourasish905athlite@gmail.com>
2026-05-17 20:30:53 +0530

    api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://codealpha-ecommerce-rppe.onrender.com/api",
+  baseURL: "https://codealpha-ecommerce-rppe.onrender.com",
 });
 
 export default API;
