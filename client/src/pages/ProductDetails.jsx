43d6718f5b596faaba22618bd4532058b1238d0f
SBR2006V <biswassourasish905athlite@gmail.com>
2026-05-18 08:49:53 +0530

    Fix product details page

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -15,7 +15,7 @@ function ProductDetails() {
         try {
           const res =
             await API.get(
-              `/products/${id}`
+              `/api/products/${id}`
             );
 
           setProduct(
